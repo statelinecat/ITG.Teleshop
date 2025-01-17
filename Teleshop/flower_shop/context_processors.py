34b99504f489bbb29a602e9fa91a11d7d28f3383
statelinecat <statelinecat@gmail.com>
2025-01-18 02:13:49 +0300

    Update context_processors.py

--- Teleshop/flower_shop/context_processors.py
+++ Teleshop/flower_shop/context_processors.py
@@ -3,26 +3,26 @@ from datetime import datetime
 from .models import Cart
 
 
-# def working_time(request):
-#     """
-#     Контекстный процессор для проверки рабочего времени.
-#     Возвращает True, если текущее время в рабочем интервале (Пн-Пт, 9:00-18:00).
-#     """
-#     now = datetime.now()
-#     is_working_time = now.weekday() < 5 and 9 <= now.hour < 18  # Пн-Пт (0-4), 9:00-18:00
-#     return {
-#         'is_working_time': is_working_time,
-#     }
-
-
 def working_time(request):
     """
     Контекстный процессор для проверки рабочего времени.
-    Для тестирования всегда возвращает False.
+    Возвращает True, если текущее время в рабочем интервале (Пн-Пт, 9:00-18:00).
     """
+    now = datetime.now()
+    is_working_time = now.weekday() < 5 and 9 <= now.hour < 18  # Пн-Пт (0-4), 9:00-18:00
     return {
-        'is_working_time': True,  # Для тестирования
+        'is_working_time': is_working_time,
     }
+
+
+# def working_time(request):
+#     """
+#     Контекстный процессор для проверки рабочего времени.
+#     Для тестирования всегда возвращает False.
+#     """
+#     return {
+#         'is_working_time': True,  # Для тестирования
+#     }
 
 def cart_quantity(request):
     """
